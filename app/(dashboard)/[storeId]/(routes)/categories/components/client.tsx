09c422d2a6476b6d39bfae829e6f48d3cf3f9e1c
Luis Eduardo Fructuoso Casanova <fructuosol@egehaina.com>
2023-12-02 08:03:35 -0400

    Complete admind store app

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { ApiAlert } from "@/components/ui/api-alert";
+
+import { columns, CategoryColumn } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
+
+interface CategoriesClientProps {
+  data: CategoryColumn[];
+}
+
+export const CategoriesClient: React.FC<CategoriesClientProps> = ({ data }) => {
+  const params = useParams();
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Categorias (${data.length})`}
+          description="Gestiona las categorías de tu tienda"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" /> Add Nueva
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Separator />
+    </>
+  );
+};
